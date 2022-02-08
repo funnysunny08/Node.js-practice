c125408320b3a931867b566c70e564fc097bf9f9
funnysunny08 <88873302+funnysunny08@users.noreply.github.com>
2022-02-08 15:44:17 +0900

    add 6.5.1

---
+++ ch06/6.5.1/learn-express/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+// GET / 라우터
+router.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/', function (req, res, next) {
+    res.locals.title = 'Express';
+    res.render('index');
+});
+
+module.exports = router;
