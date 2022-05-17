510eaa3b06f3b6220f526350e6bdf04196baffbf
funnysunny08 <88873302+funnysunny08@users.noreply.github.com>
2022-05-17 22:31:29 +0900

    12.3 ~ 12.4

--- ch12/gif-chat/socket.js
+++ ch12/gif-chat/socket.js
@@ -1,26 +1,30 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
-module.exports = (server) => {
-    const wss = new WebSocket.Server({ server });
+module.exports = (server, app) => {
+    const io = SocketIO(server, { path: '/socket.io' });
+    app.set('io', io);
+    const room = io.of('/room');
+    const chat = io.of('/chat');
 
-    wss.on('connection', (ws, req) => { // 웹소켓 연결 시
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => { // 클라이언트로부터 메시지
-            console.log(message.toString());
+    room.on('connection', (socket) => {
+        console.log('room 네임스페이스에 접속');
+        socket.on('disconnect', () => {
+            console.log('room 네임스페이스 접속 해제');
         });
-        ws.on('error', (error) => { // 에러 시
-            console.error(error);
-        });
-        ws.on('close', () => { // 연결 종료 시
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
-        });
+    });
 
-        ws.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
-            if (ws.readyState === ws.OPEN) {
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-            }
-        }, 3000);
+    chat.on('connection', (socket) => {
+        console.log('chat 네임스페이스에 접속');
+        const req = socket.request;
+        const { headers: { referer } } = req;
+        const roomId = referer
+            .split('/')[referer.split('/').length - 1]
+            .replace(/\?.+/, '');
+        socket.join(roomId);
+
+        socket.on('disconnect', () => {
+            console.log('chat 네임스페이스 접속 해제');
+            socket.leave(roomId);
+        });
     });
 };
